6fa6c306587e0a41f5cb734f7a2884fbba601e51
Daniela Alves <dani.alvesm22@gmail.com>
2024-03-25 13:17:17 -0300

    :construction: construindo o container de formulário para criar novas tarefas

--- src/containers/Formulario/index.tsx
+++ src/containers/Formulario/index.tsx
@@ -7,12 +7,12 @@ const Formulario = () => {
       <form>
         <Campo type="text" placeholder="Título" />
         <textarea placeholder="Descrição da Tarefa" />
-        <Campo type="radio" name="prioridade" id="urgente" />
+        <Campo type="radio" name="prioridade" id="urgente" />{' '}
         <label htmlFor="urgente">Urgente</label>
         <Campo type="radio" name="prioridade" id="importante" />
         <label htmlFor="urgente">importante</label>
-        <Campo type="radio" name="prioridade" id="normal" />
-        <label htmlFor="normal">Urgente</label>
+        <Campo type="radio" name="prioridade" id="normal" />{' '}
+        <label htmlFor="normal">Normal</label>
         <button type="submit">Cadastrar</button>
       </form>
     </MainContainer>
