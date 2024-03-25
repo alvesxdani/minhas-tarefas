43b1b831cb55b5c8777bbfd994e96db461f9aff7
alvesxdani <dani.alvesm22@gmail.com>
2024-03-24 21:04:02 -0300

    :sparkles: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import BarraLateral from './containers/BarraLateral'
 import ListaDeTarefas from './containers/ListaDeTarefas'
 import { Container, GlobalStyle } from './styles'
 import store from './store'
+import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
 
 function App() {
+  const rotas = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/novo',
+      element: <Cadastro />
+    }
+  ])
   return (
     <Provider store={store}>
       <GlobalStyle />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
+        {/* <BarraLateral />
+        <ListaDeTarefas /> */}
       </Container>
       Teste
     </Provider>
