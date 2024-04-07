7e9a1d114438d941cfd899e1979b2c45f6f2acac
alvesxdani <dani.alvesm22@gmail.com>
2024-04-07 16:59:23 -0300

    :broom: limpeza de linhas desnecessárias

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import BarraLateral from './containers/BarraLateral'
-import ListaDeTarefas from './containers/ListaDeTarefas'
 import { Container, GlobalStyle } from './styles'
 import store from './store'
 import Home from './pages/Home'
