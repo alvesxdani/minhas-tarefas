55e4a296b0390fcf21dab378a19184177eed112a
alvesxdani <dani.alvesm22@gmail.com>
2024-04-07 14:43:50 -0300

    :lipstick: finalização da estilização global e variável de cores

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components'
+import variaveis from './variaveis'
+import { Botao } from '../components/Tarefa/styles'
 
 export const GlobalStyle = createGlobalStyle`
 * {
@@ -12,4 +14,38 @@ export const GlobalStyle = createGlobalStyle`
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 224px auto;
+`
+
+export const MainContainer = styled.main`
+  padding: 0 40px;
+  li {
+    list-style: none;
+  }
+  height: 80vh;
+  overflow-y: scroll;
+`
+
+export const Titulo = styled.h2`
+  display: block;
+  margin: 40px 0;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+export const Campo = styled.input`
+  padding: 8px;
+  background-color: #fff;
+  border-radius: 8px;
+  font-weight: bold;
+  color: #666;
+  border-color: #666;
+  width: 100%;
+  &[type='radio'] {
+    width: fit-content;
+    margin-right: 3px;
+  }
+`
+
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.verde};
 `
